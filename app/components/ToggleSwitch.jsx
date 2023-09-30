a220ed0712f4f44771ebdd3ebd70eb3ade78ca43
christinolb <49823328+christinolb@users.noreply.github.com>
2023-09-30 14:30:34 -0400

    missing file

---
+++ app/components/ToggleSwitch.jsx
@@ -0,0 +1,10 @@
+export default function ToggleSwitch() {
+    return (
+        <>
+            <label className="relative inline-block width-[60px] height-[34px]">
+                <input type="opacity-0 width-[0] height-[0]"/>
+                <span className="position"/>
+            </label>
+        </>
+    )
+}
