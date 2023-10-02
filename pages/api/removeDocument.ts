16ad20f5539b20811e168253421bf662fb9b61c2
christinolb <49823328+christinolb@users.noreply.github.com>
2023-10-02 19:54:37 -0400

    changed remove query

--- pages/api/removeDocument.ts
+++ pages/api/removeDocument.ts
@@ -37,7 +37,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             //delete doc 
             const doc = [
                 {
-                    title: selection
+                    $title: selection
                 },
             ];
             
