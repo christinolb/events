1a72c78488d7c26f23d4c3404bbf49cc83764589
Christino L Barbosa <christinobarbosa09@gmail.com>
2023-10-03 06:44:50 -0400

    missing document

---
+++ pages/api/removeDocument.ts
@@ -0,0 +1,59 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+//solves response bug
+export const config = {
+    api: {
+      externalResolver: true,
+    },
+  }
+  
+  const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      }
+    });  
+
+export default async function handleRemove(req: NextApiRequest, res: NextApiResponse) {
+
+    const { selection } = req.body;
+
+    // run function here
+    // what does .catch(console.dir) mean?
+    removeDocument(selection).catch(console.dir)
+
+    async function removeDocument(selection: string){
+        try {
+          //connect to DB
+          await client.connect();
+      
+          //select db
+          const myDB = client.db("web_content_test");
+
+          //select cluster
+          const myColl = myDB.collection("events");
+          
+          //remove document
+          const condition = {title: selection}
+
+          await myColl.deleteOne(condition)
+          
+          
+          //return status request
+          return res.status(200).json({msg: "Event successfully removed!"})
+
+        } catch(error){
+          
+          return res.status(500).json({msg: "Failed to remove event."})
+      
+          } finally {
+          // Ensures that the client will close when you finish/error
+          await client.close();
+          }
+      }
+
+    
+}
